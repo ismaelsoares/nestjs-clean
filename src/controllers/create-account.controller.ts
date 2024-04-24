dd781358f77856c2a2d13cbed8e5d507d09f2974
Ismael Soares <ismaelsoaresdoria@gmail.com>
2024-04-24 13:46:02 -0300

    Add Rest Client

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,17 +4,28 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { hash } from "bcryptjs";
+import { z } from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type createAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 @Controller("/accounts")
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  @HttpCode(201) //Forçar o código de sucesso seja 201
-  async handle(@Body() body: any) {
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema)) //Forçar o código de sucesso seja 201
+  async handle(@Body() body: createAccountBodySchema) {
     console.log(body);
     const { name, email, password } = body;
 
